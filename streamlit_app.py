c01cac6e75490da6d4f63b5d1fb64c3c36dac1f6
Zachary Walker <45021815+qwerq33@users.noreply.github.com>
2022-11-28 12:57:58 -0500

    Update streamlit_app.py
    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,7 @@ except URLError as e:
 # new section for fruit load list
 streamlit.header("The fruit load list contains:")
 def get_fruit_load_list():
-  with my_cnc.cursor() as my_cur:
+  with my_cnx.cursor() as my_cur:
     my_cur.execute("select * from fruit_load_list")
     return my_cur.fetchall()
 
